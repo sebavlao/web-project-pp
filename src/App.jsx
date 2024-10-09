e0f652c06049246468137a835944736e0ea361c2
FrncGB <francomartingb@gmail.com>
2024-10-09 14:59:21 -0300

    VISUAL | Publicacion solicitar trabajador | USER
    Debe enviar:
    Titulo
    Categoría (tipo de trabajo)
    Descripción
    
    (También se solicitaba carga de imágenes pero, se descarta dicha implementación por el momento)

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,62 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Login } from './pages/Login'
-import { RegisterClient } from './pages/RegisterClient'
-import { RegisterWorker } from './pages/RegisterWorker'
-import {WorkerDashboard} from './pages/private/WorkerDashboard';
-import AuthAdminProvider from './hooks/adminHooks/useAuth'
-import AdminLogin from './pages/adminPages/AdminLogin'
-import AdminLayout from './components/adminComponents/Layouts/AdminLayout'
-import AdminDashboard from './pages/adminPages/usersInfoPage/AdminDashboard'
-import AdminProfile from './pages/adminPages/AdminProfile'
-import InfoUser from './pages/adminPages/usersInfoPage/InfoUser'
-import SuperAdminDashboard from './pages/adminPages/superAdminPages/SuperAdminDashboard'
-import JobsDashboard from './pages/adminPages/jobsPage/JobsDashboard'
-import LogsDashboard from './pages/adminPages/logsPage/LogsDashboard'
-import { AuthProvider } from './context/AuthContext'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { RegisterClient } from "./pages/RegisterClient";
+import { RegisterWorker } from "./pages/RegisterWorker";
+import { WorkerDashboard } from "./pages/private/WorkerDashboard";
+import AuthAdminProvider from "./hooks/adminHooks/useAuth";
+import AdminLogin from "./pages/adminPages/AdminLogin";
+import AdminLayout from "./components/adminComponents/Layouts/AdminLayout";
+import AdminDashboard from "./pages/adminPages/usersInfoPage/AdminDashboard";
+import AdminProfile from "./pages/adminPages/AdminProfile";
+import InfoUser from "./pages/adminPages/usersInfoPage/InfoUser";
+import SuperAdminDashboard from "./pages/adminPages/superAdminPages/SuperAdminDashboard";
+import JobsDashboard from "./pages/adminPages/jobsPage/JobsDashboard";
+import LogsDashboard from "./pages/adminPages/logsPage/LogsDashboard";
+import { AuthProvider } from "./context/AuthContext";
+import { JobApplication } from "./pages/private/JobApplication";
 
 function App() {
   return (
     <AuthProvider>
       <AuthAdminProvider>
         <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Home></Home>}/>
-              <Route path='/login' element={<Login/>}/>
-              <Route path='/registro-cliente' element={<RegisterClient/>}/>
-              <Route path='/registro-trabajador' element={<RegisterWorker/>}/>
-              <Route path='/dashboard-trabajador' element={<WorkerDashboard/>}/>
-              <Route path='/auth/admin-login' element={<AdminLogin />}/>
-              <Route path='/admin' element={<AdminLayout />}>
-                <Route index element={<AdminDashboard></AdminDashboard>} />
-                <Route path='/admin/profile' element={<AdminProfile></AdminProfile>} />
-                <Route path='/admin/user/:id' element={<InfoUser></InfoUser>} />
-                <Route path='/admin/jobs' element={<JobsDashboard></JobsDashboard>} />
-                <Route path='/admin/logs' element={<LogsDashboard></LogsDashboard>} />
-                <Route path='/admin/superadmin' element={<SuperAdminDashboard></SuperAdminDashboard>} />
-              </Route>
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home></Home>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro-cliente" element={<RegisterClient />} />
+            <Route path="/registro-trabajador" element={<RegisterWorker />} />
+            <Route path="/dashboard-trabajador" element={<WorkerDashboard />} />
+            <Route path="/auth/admin-login" element={<AdminLogin />} />
+            <Route
+              path="/cliente/solicitud-trabajo"
+              element={<JobApplication />}
+            />
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<AdminDashboard></AdminDashboard>} />
+              <Route
+                path="/admin/profile"
+                element={<AdminProfile></AdminProfile>}
+              />
+              <Route path="/admin/user/:id" element={<InfoUser></InfoUser>} />
+              <Route
+                path="/admin/jobs"
+                element={<JobsDashboard></JobsDashboard>}
+              />
+              <Route
+                path="/admin/logs"
+                element={<LogsDashboard></LogsDashboard>}
+              />
+              <Route
+                path="/admin/superadmin"
+                element={<SuperAdminDashboard></SuperAdminDashboard>}
+              />
+            </Route>
+          </Routes>
         </BrowserRouter>
       </AuthAdminProvider>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
