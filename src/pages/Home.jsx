6f3cdd86dc080048b620882a22b8e593d853a7d7
jvinuesa <you@example.com>
2024-10-15 11:15:39 -0300

    se agregaron botones para ir al login y al register

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,36 @@
+import { NavLink } from 'react-router-dom';
+
 export const Home = () => (
-    <main className="min-h-screen flex justify-center items-center py-6 px-4">
-      <h1 className="text-white font-bold flex relative text-3xl">Bienvenidos a la proxima aplicación para solicitar trabajo</h1>
-    </main>
-)
+  
+  <main className="min-h-screen flex flex-col justify-center items-center py-6 px-4">
+    <img 
+      src="/admin/worklink-logo-colored.png" 
+      alt="Logo de la Compañía" 
+      className="h-40 mb-4" // Tamaño del logo y margen inferior
+    />
+    <h1 className="text-white font-bold text-center text-4xl mb-6 py-6">
+      Bienvenidos a la próxima aplicación para solicitar trabajo
+    </h1>
+    <div className="flex space-x-4">
+  <NavLink 
+    to="/login"
+    className="bg-white text-black font-bold py-4 px-4 rounded"
+  >
+    Login
+  </NavLink>
+  <NavLink 
+    to="/registro-cliente"
+    className="bg-white text-black font-bold py-4 px-10 rounded"
+  >
+    Registro Cliente
+  </NavLink>
+  <NavLink 
+    to="/registro-trabajador"
+    className="bg-white text-black font-bold py-4 px-10 rounded"
+  >
+    Registro Trabajador
+  </NavLink>
+</div>
+   
+  </main>
+);
