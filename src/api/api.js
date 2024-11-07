2ae8722f01a77198c68ff869e226a23755ed3a3f
Videla Sebastián <88522430+sebavlao@users.noreply.github.com>
2024-11-06 23:58:47 -0300

    Fix URL back to fetch

--- src/api/api.js
+++ src/api/api.js
@@ -4,19 +4,19 @@ import { useAccessToken } from "../hooks/useAccessToken"
 const { getToken } = useAccessToken();
 
 export const API_JOBS = axios.create({
-    baseURL: "http://localhost:5000/api/jobs",
+    baseURL: "http://worklink.com.ar:5000/api/jobs",
     headers: {
         'x-access-token': getToken()
     }
 })
 export const API_USER = axios.create({
-    baseURL: "http://localhost:5000/api/user"
+    baseURL: "http://worklink.com.ar:5000/api/user"
 })
 export const API_PUBLIC = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "http://worklink.com.ar:5000/api"
 })
 export const API_CURRENT_USER = axios.create({
-    baseURL: "http://localhost:5000/api/user/me",
+    baseURL: "http://worklink.com.ar:5000/api/user/me",
     headers: {
         'x-access-token': getToken()
     }
